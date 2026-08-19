d877c75c78cb024465c037eda82687f89e6a20e5
Namul <namul@Namuls-MacBook-Air.local>
2026-08-19 21:28:36 +0600

    mod: moved 2 home page section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import AgricultureIntelligence from "@/components/AgricultureIntelligence";
-import ServicesSection from "@/components/ServicesSection";
+import AgricultureIntelligence from "@/components/home/AgricultureIntelligence";
+import ServicesSection from "@/components/home/ServicesSection";
 import Image from "next/image";
 
 export default function Home() {
