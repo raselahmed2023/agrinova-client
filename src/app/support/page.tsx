d0a09cea24661713f2ae2b636a4bd4e7953e7cff
abdul-korim-web <moneyschorsing@gmail.com>
2026-09-02 22:28:04 +0600

    feat(support): add SupportHero component UI

---
+++ src/app/support/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  Navbar  from '@/components/shared/Navbar';
+import SupportHero from '@/components/support/SupportHero';
+
+export default function page() {
+  return (
+   <div>
+     <Navbar/>
+    <SupportHero/>
+   </div>
+  )
+}
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -11,7 +11,7 @@ const navLinks = [
   { label: "Marketplace", href: "/marketplace" },
   { label: "Consultant", href: "/consultant" },
   { label: "Blog", href: "/blog" },
-  { label: "Contact", href: "/contact" },
+  { label: "Support", href: "/support" },
 ];
 
 export default function Navbar() {
