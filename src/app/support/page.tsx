ce4ebcb84856f9116157d49c9aff7a482ffedf2d
abdul-korim-web <moneyschorsing@gmail.com>
2026-09-02 22:39:03 +0600

    feat(support): add SupportBenefits component UI

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import  Navbar  from '@/components/shared/Navbar';
 import SupportHero from '@/components/support/SupportHero';
 import HowItWorks from '@/components/support/HowItWorks';
+import SupportBenefits from '@/components/support/SupportBenefits';
 
 export default function page() {
   return (
@@ -9,6 +10,7 @@ export default function page() {
      <Navbar/>
     <SupportHero/>
     <HowItWorks/>
+    <SupportBenefits/>
    </div>
   )
 }
